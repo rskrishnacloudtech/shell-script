54fe4a726f5fe91c4ca093bb641e7777835da6da
Siva <rsk.sfdctraining1@gmail.com>
2024-07-27 17:53:35 +0530

    shell script

--- 13-install-packages.sh
+++ 13-install-packages.sh
@@ -37,7 +37,7 @@ fi
 for i in $@
 do
     echo "Package to install... $i"
-    dng list installed $i &>> LOGFILE_NAME
+    dnf list installed $i &>> LOGFILE_NAME
     if [ $? -ne 0 ]
     then
         echo -e "$i package is already installed... $Y SKIPPING $N" &>> LOGFILE_NAME
