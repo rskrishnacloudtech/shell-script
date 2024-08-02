decf25041e9c0fd0e2cfa81753286ef16f67ed77
Siva <rsk.sfdctraining1@gmail.com>
2024-08-02 19:20:16 +0530

    Shell Script

--- 14.script1.sh
+++ 14.script1.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
+# Variable in script1.
 COURSE="Learing DevOps in script1"
 
+# Printing the variable value and PID of script1.
 echo "Course from script1: $COURSE"
 echo "Process ID of script1: $$"
 
+# Calling the script2 directly.
 #./15.script2.sh
+
+# Calling the script2  by sourcing.
 source ./15.script2.sh
 
+# Printing the variable value and PID of script1 after calling script2.
 echo "Course from script1 after calling script2: $COURSE"
 echo "Process ID of script1 after calling script2: $$"
--- 15.script2.sh
+++ 15.script2.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+# Variable in script2.
 COURSE="Learing SALESFORCE in script2"
 
+# Printing the variable value and PID of script2.
 echo "Course from script2: $COURSE"
 echo "Process ID of script2: $$"
