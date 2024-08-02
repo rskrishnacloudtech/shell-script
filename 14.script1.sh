5fe91a588a735dff9c49a6f1988cc34428bcff81
Siva <rsk.sfdctraining1@gmail.com>
2024-08-02 19:24:05 +0530

    Shell Script

--- 14.script1.sh
+++ 14.script1.sh
@@ -7,10 +7,10 @@ COURSE="Learing DevOps in script1"
 echo "Course from script1: $COURSE"
 echo "Process ID of script1: $$"
 
-# Calling the script2 directly.
+# Calling the script2 directly. In this case, variable value of script1 will NOT be changed and PID of both the scripts will be different.
 #./15.script2.sh
 
-# Calling the script2  by sourcing.
+# Calling the script2  by sourcing. In this case, variable value of script1 will be changed and PID of both the scripts will be same.
 source ./15.script2.sh
 
 # Printing the variable value and PID of script1 after calling script2.
