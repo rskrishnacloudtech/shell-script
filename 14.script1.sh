b4c0ef35c09e166346d40273ea90566846060afe
Siva <rsk.sfdctraining1@gmail.com>
2024-08-02 19:12:49 +0530

    Shell Script

--- 14.script1.sh
+++ 14.script1.sh
@@ -5,7 +5,7 @@ course="Learing DevOps in script1"
 echo "Course from script1: $COURSE"
 echo "Process ID of script1: $$"
 
-./script2.sh
+./15.script2.sh
 
 echo "Course from script1 after calling script2: $COURSE"
 echo "Process ID of script1 after calling script2: $$"
