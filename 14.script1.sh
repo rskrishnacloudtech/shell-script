ea7a40bb0f72f49556fecee4635a30e34d0854d1
Siva <rsk.sfdctraining1@gmail.com>
2024-08-02 18:45:22 +0530

    Shell Script

---
+++ 14.script1.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+course="Learing DevOps in script1"
+
+echo "Course from script1: $COURSE"
+echo "Process ID of script1: $$"
+
+./script2.sh
+
+echo "Course from script1 after calling script2: $COURSE"
+echo "Process ID of script1 after calling script2: $$"
---
+++ 16.delete-old-files.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Getting the source directory to find and delete the old files.
+source_dir=""
+
+# Declaring a color codes to used in the logs.
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+# Check that directory is exists or not.
+if [ -d $source_dir ] 
+then
+    echo -e "$R $source_dir is not exists. Please check manually once. $N"
+else
+    echo -e "$G $source_dir is exists. File will be found and deleted as per the requirement $N"
+    exit 1
+fi
+
+# Finding the files in the above $source_dir which are created before 5 days with name .log.
+files_to_delete=$(find $source_dir -name "*.log" -mtime +5)
+
+echo -e "$Y Files found to delete: $files_to_delete $N"
+
+# Deleting the files.
+while IFS=read -r line
+do
+    echo -e "$Y Deleting the files... $N"
+    rm -rf $line
+done <<< $files_to_delete
