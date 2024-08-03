c2493b0fe602f1806722c5b0528860a3bca6321e
Siva <rsk.sfdctraining1@gmail.com>
2024-08-03 08:07:35 +0530

    Shell Script

--- 17.files-backup.sh
+++ 17.files-backup.sh
@@ -6,4 +6,14 @@ fromFolder=/home/ec2-user/fromfolder/
 toFolder=/home/ec2-user/tofolder/
 zipFileName="file-backup-$timestamp.zip"
 
-zip -r "$toFolder/$zipFileName.zip" $fromFolder
+# Zipping the folder and moving into another folder.
+zip -r "$toFolder/$zipFileName" $fromFolder
+
+#
+ls -l $toFolder | grep zipFileName
+if [ $? -eq 0 ]
+then
+    echo "Files has been backed up successfully."
+else
+    echo "Files has NOT been backed up. Backup FAILED. Please review it once."
+fi
