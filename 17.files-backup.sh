5f4e1afd0e13e1e54b9b0648d88344fac974c4bb
Siva <rsk.sfdctraining1@gmail.com>
2024-08-03 08:01:59 +0530

    Shell Script

--- 17.files-backup.sh
+++ 17.files-backup.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+# Forming a time stamp
 timestamp=$(date +%F-%H-%M-%S)
 fromFolder=/home/ec2-user/fromfolder/
 toFolder=/home/ec2-user/tofolder/
 zipFileName="file-backup-$timestamp.zip"
 
-zip -r /home/ec2-user/tofolder/$zipFileName.zip /home/ec2-user/fromfolder/
+zip -r "$toFolder/$zipFileName.zip" $fromFolder
