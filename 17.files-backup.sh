5a2436abf8d52c5d9e0fe73e172b0babae821c78
Siva <rsk.sfdctraining1@gmail.com>
2024-08-03 08:00:17 +0530

    Shell Script

--- 17.files-backup.sh
+++ 17.files-backup.sh
@@ -5,4 +5,4 @@ fromFolder=/home/ec2-user/fromfolder/
 toFolder=/home/ec2-user/tofolder/
 zipFileName="file-backup-$timestamp.zip"
 
-zip -r /home/ec2-user/tofolder/data.zip /home/ec2-user/fromfolder/
+zip -r /home/ec2-user/tofolder/$zipFileName.zip /home/ec2-user/fromfolder/
